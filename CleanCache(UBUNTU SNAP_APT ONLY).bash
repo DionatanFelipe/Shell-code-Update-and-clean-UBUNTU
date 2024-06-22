7b51d744c9c2db22d93836b26d4c051e5b5ac631
DionatanFelipe <122566200+DionatanFelipe@users.noreply.github.com>
2024-06-22 17:59:52 -0300

    Update and rename CleanCache(UBUNTU SNAP_APT ONLY).bash to CleanSNAP.bash

--- CleanCache(UBUNTU SNAP_APT ONLY).bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Clean cache
-# Limpeza de sistema
-
- sudo apt clean && sudo apt autoremove && 
- sudo rm -rf /var/cache/snapd 
---
+++ CleanSNAP.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script para limpeza do sistema
+#Clean Snap
+# Limpar cache do APT
+sudo apt clean
+sudo apt autoremove -y
